e3a229f1923eeef5448a466fecdd7f7771cc8f02
kyungseop <kyungseop@gmail.com>
2022-09-30 06:22:22 +0900

    change required fields

--- api/src/main/java/com/example/api/paper/model/command/request/PaperUpdateRequest.java
+++ api/src/main/java/com/example/api/paper/model/command/request/PaperUpdateRequest.java
@@ -1,6 +1,7 @@
 package com.example.api.paper.model.command.request;
 
 import javax.validation.constraints.NotBlank;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -13,10 +14,8 @@ public class PaperUpdateRequest {
 
     @NotBlank
     private String title;
-    @NotBlank
     private String subTitle;
     private String imageUrl;
-    @NotBlank
     @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private String publishDate;
 }
